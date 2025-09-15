825958e6c5262b8cff56b211f2919c64ed876134
Mansurkhoja <mansur.atm@gmail.com>
2025-09-15 21:54:50 +0500

    Add Character model with JSON serialization

---
+++ lib/models/character.dart
@@ -0,0 +1,39 @@
+class Character {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String gender;
+  final String locationName;
+  final String image;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.gender,
+    required this.locationName,
+    required this.image,
+  });
+
+  factory Character.fromJson(Map<String, dynamic> json) => Character(
+    id: json['id'],
+    name: json['name'],
+    status: json['status'],
+    species: json['species'],
+    gender: json['gender'],
+    locationName: json['location']['name'],
+    image: json['image'],
+  );
+
+  // Map<String, dynamic> toJson() => {
+  //   'id': id,
+  //   'name': name,
+  //   'status': status,
+  //   'species': species,
+  //   'locationName': locationName,
+  //   'gender': gender,
+  //   'image': image,
+  // };
+}
