9f491772a691e49cd7178cbf5cfd94ccd33fff9e
Mansurkhoja <mansur.atm@gmail.com>
2025-09-15 22:58:47 +0500

    Fetch Characters

--- lib/screens/characters_screen.dart
+++ lib/screens/characters_screen.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty/models/character.dart';
+import 'package:rick_and_morty/repositories/character/character_repository.dart';
 import 'package:rick_and_morty/widgets/character_card.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  List<Character> _characters = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchCharacters();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 2,
-      itemBuilder: (context, index) => CharacterCard(),
+      itemCount: _characters.length,
+      itemBuilder: (context, index) =>
+          CharacterCard(character: _characters[index]),
     );
+  }
+
+  Future<void> _fetchCharacters() async {
+    final fetched = await CharacterRepository().fetchCharacters();
+    setState(() {
+      _characters = fetched;
+    });
   }
 }
