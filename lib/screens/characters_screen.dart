d6fff616411f34ab2e496d5c7b7b64144fea740a
Mansurkhoja <mansur.atm@gmail.com>
2025-09-17 15:07:11 +0500

    Fix when click statusbar on iphone scroll to top

--- lib/screens/characters_screen.dart
+++ lib/screens/characters_screen.dart
@@ -3,48 +3,37 @@ import 'package:provider/provider.dart';
 import 'package:rick_and_morty/providers/characters_provider.dart';
 import 'package:rick_and_morty/widgets/character_card.dart';
 
-class CharactersScreen extends StatefulWidget {
+class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
-
-  @override
-  State<CharactersScreen> createState() => _CharactersScreenState();
-}
-
-class _CharactersScreenState extends State<CharactersScreen> {
-  late final ScrollController _scrollController;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController()
-      ..addListener(() {
-        final provider = Provider.of<CharactersProvider>(
-          context,
-          listen: false,
-        );
-        if (_scrollController.position.pixels >=
-                _scrollController.position.maxScrollExtent - 200 &&
-            !provider.isLoading) {
-          provider.fetchNextPage();
-        }
-      });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CharactersProvider>(
       builder: (context, provider, _) {
-        return ListView.builder(
-          controller: _scrollController,
-          itemCount: provider.characters.length,
-          itemBuilder: (context, index) {
-            final character = provider.characters[index];
-            return CharacterCard(
-              character: character,
-              isFavorite: provider.isFavorite(character.id),
-              onPressFavorite: () => provider.toggleFavorite(character),
-            );
+        return NotificationListener<ScrollNotification>(
+          onNotification: (scrollNotification) {
+            if (scrollNotification is ScrollEndNotification) {
+              final metrics = scrollNotification.metrics;
+              if (metrics.pixels >= metrics.maxScrollExtent - 2000 &&
+                  !provider.isLoading) {
+                provider.fetchNextPage();
+                debugPrint('Fetching next page...');
+              }
+            }
+            return false;
           },
+          child: ListView.builder(
+            primary: true,
+            itemCount: provider.characters.length,
+            itemBuilder: (context, index) {
+              final character = provider.characters[index];
+              return CharacterCard(
+                character: character,
+                isFavorite: provider.isFavorite(character.id),
+                onPressFavorite: () => provider.toggleFavorite(character),
+              );
+            },
+          ),
         );
       },
     );
