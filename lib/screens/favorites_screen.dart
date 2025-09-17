a5aa2211b665eceb2fdd00b54e3a7512aea33766
Mansurkhoja <mansur.atm@gmail.com>
2025-09-17 12:48:24 +0500

    Implement FavoritesScreen to display favorite characters using CharactersProvider

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rick_and_morty/providers/characters_provider.dart';
+import 'package:rick_and_morty/widgets/character_card.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(strokeWidth: 22);
+    return Consumer<CharactersProvider>(
+      builder: (context, provider, _) {
+        final favorites = provider.favorites;
+        if (favorites.isEmpty) {
+          return const Center(child: Text('No favorites yet.'));
+        } else {
+          return ListView.builder(
+            itemCount: favorites.length,
+            itemBuilder: (context, index) {
+              final character = favorites[index];
+              return CharacterCard(
+                character: character,
+                isFavorite: provider.isFavorite(character.id),
+                onPressFavorite: () => provider.toggleFavorite(character),
+              );
+            },
+          );
+        }
+      },
+    );
   }
 }
