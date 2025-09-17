ae71430969b36fab0812f60731444d0700e0f8d9
Mansurkhoja <mansur.atm@gmail.com>
2025-09-17 13:27:42 +0500

    Refactor CharactersScreen to use StatefulWidget and implement infinite scrolling to load page

--- lib/screens/characters_screen.dart
+++ lib/screens/characters_screen.dart
@@ -3,14 +3,39 @@ import 'package:provider/provider.dart';
 import 'package:rick_and_morty/providers/characters_provider.dart';
 import 'package:rick_and_morty/widgets/character_card.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late final ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        final provider = Provider.of<CharactersProvider>(
+          context,
+          listen: false,
+        );
+        if (_scrollController.position.pixels >=
+                _scrollController.position.maxScrollExtent - 200 &&
+            !provider.isLoading) {
+          provider.fetchNextPage();
+        }
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Consumer<CharactersProvider>(
       builder: (context, provider, _) {
         return ListView.builder(
+          controller: _scrollController,
           itemCount: provider.characters.length,
           itemBuilder: (context, index) {
             final character = provider.characters[index];
